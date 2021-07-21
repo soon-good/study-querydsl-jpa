cc7c6bd603c3d7bfa07d806a3a54b2a36d96a2cb
gosgjung <playmymind@daum.net>
2021-07-22 06:42:06 +0900

    스프링부트의 웹 요청 처리스의 엔티티매니저 내의 영속성컨텍스트가 살아있는 주기를 파악하기 위한 용도의 예제 추가  : 빈의 스코프와도 연관성이 있는지 테스트해볼예정  : 각 테스트 결과를 캡처하고, 결과문서를 만들어서 기록으로 남겨둘 예정

---
+++ codes/JPA-Basic/Entity-Manager-Cache/src/main/java/io/study/jpa/entitycache/GenerationStrategyApplication.java
@@ -0,0 +1,13 @@
+package io.study.jpa.entitycache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationStrategyApplication.class, args);
+	}
+
+}
