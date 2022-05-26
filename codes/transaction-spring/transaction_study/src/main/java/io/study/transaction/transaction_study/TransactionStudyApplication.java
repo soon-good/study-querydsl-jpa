207c9372bf1521de9a45c16cbc1e23fe3dc11471
soon.good <playmymind@daum.net>
2022-05-26 23:00:04 +0900

    트랜잭션 개념정리
    예제 프로젝트 추가

---
+++ codes/transaction-spring/transaction_study/src/main/java/io/study/transaction/transaction_study/TransactionStudyApplication.java
@@ -0,0 +1,13 @@
+package io.study.transaction.transaction_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionStudyApplication.class, args);
+	}
+
+}
---
+++ codes/transaction-spring/transaction_study/src/test/java/io/study/transaction/transaction_study/TransactionStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.transaction.transaction_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
