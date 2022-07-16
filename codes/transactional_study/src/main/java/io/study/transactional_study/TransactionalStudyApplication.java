e7927e12d497d3d8f39042457db5ac57d248dde4
soon.good <playmymind@daum.net>
2022-07-17 08:53:35 +0900

    트랜잭션 예제

---
+++ codes/transactional_study/src/main/java/io/study/transactional_study/TransactionalStudyApplication.java
@@ -0,0 +1,13 @@
+package io.study.transactional_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalStudyApplication.class, args);
+    }
+
+}
---
+++ codes/transactional_study/src/test/java/io/study/transactional_study/TransactionalStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.transactional_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
