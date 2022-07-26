3b2427a54304f81f5284b1335769c7f08e37f734
soon.good <playmymind@daum.net>
2022-07-27 06:38:47 +0900

    커밋롤백 테스트 예제 2) 언체크드 익셉션 발생시 커밋 여부 테스트

--- codes/transactional_study/src/test/java/io/study/transactional_study/commit_rollback/unchecked_exception/BookServiceUncheckedExceptionTest1.java
+++ codes/transactional_study/src/test/java/io/study/transactional_study/commit_rollback/unchecked_exception/BookServiceUncheckedExceptionTest1.java
@@ -1,2 +1,41 @@
-package io.study.transactional_study.commit_rollback.checked_exception;public class BookServiceUncheckedExceptionTest1 {
+package io.study.transactional_study.commit_rollback.unchecked_exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+public class BookServiceUncheckedExceptionTest1 {
+
+    @Autowired
+    BookService bookService;
+
+    @Test
+    public void 언체크드_예외_발생시_트랜잭션은_롤백된다(){
+        Assertions.assertThatThrownBy(() -> bookService.throwUncheckedException())
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @TestConfiguration
+    static class InlineConfiguration{
+        @Bean
+        BookService bookService(){
+            return new BookService();
+        }
+    }
+
+    @Slf4j
+    static class BookService{
+        @Transactional
+        public void throwUncheckedException(){
+            log.info("언체크드 익셉션(RuntimeException) throw 하겠음");
+            throw new RuntimeException();
+        }
+    }
 }
