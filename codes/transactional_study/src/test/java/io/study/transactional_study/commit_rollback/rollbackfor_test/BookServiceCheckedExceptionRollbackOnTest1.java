cc5d9cd82fbeeb0f875e1b464324e046b187ad18
soon.good <playmymind@daum.net>
2022-07-27 06:53:18 +0900

    커밋롤백 테스트 예제 3) 체크드 익셉션 발생시 rollbackOn 에 대해서  예외적으로 롤백하는지  테스트

---
+++ codes/transactional_study/src/test/java/io/study/transactional_study/commit_rollback/rollbackfor_test/BookServiceCheckedExceptionRollbackOnTest1.java
@@ -0,0 +1,45 @@
+package io.study.transactional_study.commit_rollback.rollbackfor_test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+public class BookServiceCheckedExceptionRollbackOnTest1 {
+
+    @Autowired
+    BookService bookService;
+
+    @Test
+    public void 체크드_예외_메서드_호출시_rollbackOn에_등록한_타입에_대해서는_체크드예외_이더라도_트랜잭션_롤백을_해야한다(){
+        Assertions.assertThatThrownBy(() -> bookService.rollbackOnMethod())
+                .isInstanceOf(JustException.class);
+    }
+
+    @TestConfiguration
+    static class InlineConfiguration{
+        @Bean
+        public BookService bookService(){
+            return new BookService();
+        }
+    }
+
+    static class JustException extends Exception{
+
+    }
+
+    @Slf4j
+    static class BookService{
+        @Transactional(rollbackOn = JustException.class)
+        public void rollbackOnMethod() throws Exception{
+            log.info("rollbackOnMethod 메서드 호출");
+            throw new JustException();
+        }
+    }
+}
