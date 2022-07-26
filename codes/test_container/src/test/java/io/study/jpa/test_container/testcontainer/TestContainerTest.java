ca0b01a9ba8ac610e161c2c61416d222f3976dc3
soon.good <playmymind@daum.net>
2022-07-27 04:07:33 +0900

    testcontainer 예제추가

---
+++ codes/test_container/src/test/java/io/study/jpa/test_container/testcontainer/TestContainerTest.java
@@ -0,0 +1,25 @@
+package io.study.jpa.test_container.testcontainer;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
+
+@Testcontainers
+@SpringBootTest
+public class TestContainerTest {
+
+    static final DockerComposeContainer container;
+
+    static{
+        container = new DockerComposeContainer(new File("src/test/resources/docker/docker-compose/docker-compose.yml"));
+        container.start();
+    }
+
+    @Test
+    public void TEST_DOCKER_COMPOSE_CONTAINER_LOADING(){
+
+    }
+}
