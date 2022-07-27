fd755f3d334638f67e54049ac2517c4b2252f729
soon.good <playmymind@daum.net>
2022-07-28 06:11:23 +0900

    etc) 지금은 사용하지 않는 예전에 사용하던 파일이지만, 브랜치 청소를 위해 커밋처리

--- codes/transactional_study/src/test/java/io/study/transactional_study/hierachy_test/ExtendedBookService.java
+++ codes/transactional_study/src/test/java/io/study/transactional_study/hierachy_test/ExtendedBookService.java
@@ -1,2 +1,19 @@
-package io.study.transactional_study.hierachy_test;public class BookChildService {
+package io.study.transactional_study.hierachy_test;
+
+public class ExtendedBookService extends BaseBookService{
+    
+    public void bookServiceStart(){
+        System.out.println("bookService 시작");
+    }
+
+    public void bookServiceEnd(){
+        System.out.println("bookService 종료");
+    }
+
+    public void somethingTodo(){
+        bookServiceStart();
+        printMessage(); // 부모객체의 메서드
+        bookServiceEnd();
+    }
+
 }
